a0b68203cc3321e57d8a7da233762124dad748c8
Kaden16 <kadenbourne16@gmail.com>
2024-09-14 23:57:50 +0000

    No massive change

--- frontend/src/components/affilaiteDashboard.jsx
+++ frontend/src/components/affilaiteDashboard.jsx
@@ -1,23 +1,25 @@
 import { Route, Routes, Link } from "react-router-dom";
 import '../stylesheet/affiliateDashboard.css'
-import Item from './affiliateItems.jsx'
+import Item from '../dashboardcomponents/affiliateItems.jsx'
 import Orders from '../dashboardcomponents/affiliatemeOrders.jsx'
 
 function Dashboard() {
   return (
     <div className="dashboard-container">
-      <div className="sidebar">
-        <h2>Dashboard</h2>
-        <ul>
-          <li><Link to="/dashboard/items">Items</Link></li>
-          <li><Link to="/dashboard/orders">Orders</Link></li>
-        </ul>
-      </div>
-      <div className="content">
-        <Routes>
-          <Route path="items" element={<Item />} />
-          <Route path="orders" element={<Orders/>}></Route>
-        </Routes>
+      <div className = "PageContainer">
+        <div className="sidebar">
+          <h2>Dashboard</h2>
+          <ul>
+            <li><Link to="/dashboard/items">Items</Link></li>
+            <li><Link to="/dashboard/orders">Orders</Link></li>
+          </ul>
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="items" element={<Item />} />
+            <Route path="orders" element={<Orders/>}></Route>
+          </Routes>
+        </div>
       </div>
     </div>
   );
